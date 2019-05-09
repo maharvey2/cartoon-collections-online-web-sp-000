86ea2f74cca276b53797442aa03dbc586db70966
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-09 21:09:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 
 def long_planeteer_calls# code an argument here
   # Your code here
-end
+  planeteer_calls.any? { |call| call.length > 4 }
+ end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
