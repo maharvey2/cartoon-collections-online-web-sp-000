4100d176edfa02cf4380e7bf75c60510c60a1cb7
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-09 21:12:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,10 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
 
  def find_the_cheese(potentially_cheesy_items)
    cheeses = %w[gouda cheddar camembert]
- 
+
    potentially_cheesy_items.find do |maybe_cheese|
      cheeses.include?(maybe_cheese)
    end
  end
- 
+
  find_the_cheese(potentially_cheesy_items)
