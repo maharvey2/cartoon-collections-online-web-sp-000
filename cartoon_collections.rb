32098d65a6f2fab8f15a482c20bcfdbbf3d8544e
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-09 21:12:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,17 +12,17 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map { |call| call.capitalize + '!' }
   end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.any? { |call| call.length > 4 }
  end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheesy.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
-  end
-end
-
-find_the_cheese(potentially_cheesy_items)
+ def find_the_cheese(potentially_cheesy_items)
+   cheeses = %w[gouda cheddar camembert]
+ 
+   potentially_cheesy_items.find do |maybe_cheese|
+     cheeses.include?(maybe_cheese)
+   end
+ end
+ 
+ find_the_cheese(potentially_cheesy_items)
