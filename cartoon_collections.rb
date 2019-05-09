be059a3396bc473690570b9b86944afa9a032360
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-09 21:16:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,11 @@ dwarves = %w[
   Sleepy
   Happy
 ]
-def roll_call_dwarves(dwarfs)# code an argument here
-  # Your code here
-  def roll_call_dwarves(dwarves)
-    dwarves.each_with_index do |dwarf, index|
-      puts "#{index + 1}. #{dwarf}"
-    end
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
   end
+end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
